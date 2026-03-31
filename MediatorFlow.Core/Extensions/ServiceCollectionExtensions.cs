f69ac06b58d8c7ea84f5d7fdef3c5e2999f71adf
Oladimeji Williams <oladimejiwilliams@outlook.com>
2026-03-31 07:45:30 +0100

    added DI registration helpers

---
+++ MediatorFlow.Core/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,45 @@
+using MediatorFlow.Core.Abstractions;
+using MediatorFlow.Core.Internal;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace MediatorFlow.Core.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddMediatorFlow(this IServiceCollection services, Assembly[]? assemblies = null)
+    {
+        assemblies ??= AppDomain.CurrentDomain.GetAssemblies();
+
+        // Register all IRequestHandler<,>
+        var requestHandlerTypes = assemblies
+            .SelectMany(a => a.GetTypes())
+            .Where(t => !t.IsAbstract && !t.IsInterface)
+            .SelectMany(t => t.GetInterfaces()
+                .Where(i => i.IsGenericType && i.GetGenericTypeDefinition() == typeof(IRequestHandler<,>))
+                .Select(i => new { Interface = i, Implementation = t }));
+
+        foreach (var handler in requestHandlerTypes)
+        {
+            services.AddTransient(handler.Interface, handler.Implementation);
+        }
+
+        // Register all INotificationHandler<>
+        var notificationHandlerTypes = assemblies
+            .SelectMany(a => a.GetTypes())
+            .Where(t => !t.IsAbstract && !t.IsInterface)
+            .SelectMany(t => t.GetInterfaces()
+                .Where(i => i.IsGenericType && i.GetGenericTypeDefinition() == typeof(INotificationHandler<>))
+                .Select(i => new { Interface = i, Implementation = t }));
+
+        foreach (var handler in notificationHandlerTypes)
+        {
+            services.AddTransient(handler.Interface, handler.Implementation);
+        }
+
+        // Register the Mediator
+        services.AddSingleton<IMediator, Mediator>();
+
+        return services;
+    }
+}
